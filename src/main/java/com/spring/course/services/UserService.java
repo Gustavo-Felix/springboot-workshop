3f3428ba87dfca5cce0db4d767ef692300c3f75e
Gustavo_Félix <gustavofelix2007@gmail.com>
2025-10-22 09:51:01 -0300

    feat: Service layer, component registration

---
+++ src/main/java/com/spring/course/services/UserService.java
@@ -0,0 +1,28 @@
+package com.spring.course.services;
+
+import ch.qos.logback.core.spi.ErrorCodes;
+import com.spring.course.entities.User;
+import com.spring.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.get();
+    }
+
+}
