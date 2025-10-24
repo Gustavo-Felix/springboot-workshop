eb1488bb02f0ca89b8af32a0e7569471bb1ce36c
Gustavo_Félix <gustavofelix2007@gmail.com>
2025-10-24 08:20:33 -0300

    feat: Order entity, Instant, ISO 8601

---
+++ src/main/java/com/spring/course/repositories/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.spring.course.repositories;
+
+import com.spring.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
---
+++ src/main/java/com/spring/course/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.spring.course.services;
+
+import com.spring.course.entities.Order;
+import com.spring.course.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public Order findById(Long id) {
+        Optional<Order> order = orderRepository.findById(id);
+        return order.get();
+    }
+}
