9a63122a863a444a0e6dee9cda5951f5ee2cdaef
Gustavo_Félix <gustavofelix2007@gmail.com>
2025-10-30 10:57:25 -0300

    feat: Exception handling - update

--- src/main/java/com/spring/course/services/UserService.java
+++ src/main/java/com/spring/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.spring.course.entities.User;
 import com.spring.course.repositories.UserRepository;
 import com.spring.course.services.exceptions.DatabaseException;
 import com.spring.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -43,9 +44,13 @@ public class UserService {
     }
 
     public User update(Long id, User newUser) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, newUser);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, newUser);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User newUser) {
